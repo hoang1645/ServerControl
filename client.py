08d41e6148e7603652c3d17d0f37ed33bc777a7f
hoang1645 <60732952+hoang1645@users.noreply.github.com>
2021-10-11 22:20:20 +0700

    cloning from the midterm git

---
+++ client.py
@@ -0,0 +1,162 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+import socket
+import commander
+import threading
+from registerGUI import *
+import keylogGUI
+import processGUI
+import appGUI
+import loadimage
+from time import sleep
+class Client(object):
+    def __init__(self):
+        """Creates the interface window"""
+        self.root = Tk()
+        self.root.title("Client")
+
+        #mainframe
+        self.mainframe = ttk.Frame(self.root, padding="10 10 25 25")
+        self.mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.root.resizable(FALSE, FALSE)
+
+        #IP addr. input space
+        self.ip_addr = StringVar()
+        ip_addr_entry = ttk.Entry(self.mainframe, width=40, textvariable=self.ip_addr)
+        ttk.Label(self.mainframe, text='IP Addr.').grid(column=1, row=1, sticky=(W,E))
+        ip_addr_entry.grid(column=1,row=2,sticky=(W,E))
+
+        #Port input space
+        self.port = StringVar()
+        port_entry = ttk.Entry(self.mainframe, width=10, textvariable=self.port)
+        ttk.Label(self.mainframe, text='Port').grid(column=2, row=1, sticky=(W,E))
+        port_entry.grid(column=2, row=2, sticky=(W,E))
+        
+        #Connect button
+        connectButton = ttk.Button(self.mainframe, text='Connect', command=self.Connect)
+        connectButton.grid(column=3, row=2, sticky=(E))
+
+        #Functions
+        ttk.Label(self.mainframe, text='Command').grid(column=1,row=3,sticky=(W,E))
+        self.func = StringVar()
+        funcEntry = ttk.Combobox(self.mainframe, textvariable=self.func, width=40)
+        funcEntry['values'] = ("Show running processes", "Show running apps", "Shutdown", "Screen capture"
+                                , "Keylog", "Edit registries")
+        funcEntry.state(["readonly"])
+        funcEntry.grid(column=1, row=4, sticky=(S))
+
+        #Confirmation button
+        self.confButton = ttk.Button(self.mainframe, text='Go', command=self.act, state=DISABLED)
+        self.confButton.grid(column=3, row=4, sticky=(E,S))
+
+        subframe = ttk.Frame(self.root, padding='3 3 12 12')
+        subframe.grid(row=1, column=0)
+
+        self.State = ttk.Label(subframe, text='Not connected')
+        self.State.grid(column=1, row=1)
+
+        for child in self.mainframe.winfo_children(): 
+            child.grid_configure(padx=5, pady=5)
+        pass
+    connection = None
+    #Connect to the server
+    def Connect(self):
+        try:
+            self.IP = str(self.ip_addr.get())
+            self.port_no = int(self.port.get())
+            self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.connection.connect((self.IP, self.port_no))
+            self.connection.send('Hello'.encode())
+
+
+            self.State['text'] = "Connected to server: " + self.IP + ":" + str(self.port_no) + "."
+            self.confButton['state'] = NORMAL
+        except:
+            messagebox.showerror(title='Connect error', message='An error occurred while trying to connect to the address ' + 
+                            self.IP + ":" + str(self.port_no) + ".")
+            self.confButton['state'] = DISABLED
+            
+        
+    def act(self):
+        func = self.func.get()
+        if func == "Show running processes":
+            self.command_ShowProcess()
+        elif func == "Show running apps":
+            self.command_ShowApps()
+        elif func == "Shutdown":
+            self.command_Shutdown()
+        elif func == "Screen capture":
+            self.command_CaptureScreen()
+        elif func == "Keylog":
+            self.command_Keylog()
+        elif func == "Edit registries":
+            self.command_RegEdit()
+
+    
+    def command_Shutdown(self):
+        cmd = commander.ShutdownCMD(self.root)
+        cmd.NewInstance()
+        if cmd.isExcuted==False:
+            cmd.command = 'Nope'
+        else:
+            cmd.command = 'SHUTDOWN'
+        command = cmd.command + " " + cmd.delay_time.get()
+        print(command)
+        self.sendToServer(command)
+        
+        pass
+
+    def command_CaptureScreen(self):
+        cmd = 'CAPSCR'
+        print(cmd)
+        self.sendToServer(cmd)
+        scrshot = open("capture.png", 'wb')
+        while True:
+            data = self.connection.recv(1024)
+            if len(data) < 1024:
+                scrshot.write(data)
+                break
+            scrshot.write(data)
+        ins = loadimage.WindowScreenShot('capture.png',Toplevel(self.root))
+        scrThread =threading.Thread(target=ins.loadImg())
+        scrThread.start()
+        scrshot.close()
+    def command_ShowProcess(self):
+        ins = processGUI.Process(self.root,self.IP,self.port_no)
+        processThread=threading.Thread(target=ins.loadProcess())
+        processThread.start()
+    def command_ShowApps(self):
+        ins = appGUI.App(self.root,self.IP,self.port_no)
+        processThread=threading.Thread(target=ins.loadApp())
+        processThread.start()
+    def command_RegEdit(self):
+        regEdit = RegistryWindow(Toplevel(),self.IP,self.port_no)
+        regeditThread = threading.Thread(target=regEdit.loadReg(res='750x500'))
+        regeditThread.start()
+    
+    def command_Keylog(self):
+        keyloggerGUI = keylogGUI.KeyloggerWindow(Toplevel(),self.IP,self.port_no)
+        keylogThread = threading.Thread(target=keyloggerGUI.loadKeyLog())
+        keylogThread.start()
+       
+
+    #Ham gui toi server
+    def sendToServer(self,Str):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((self.IP, self.port_no))
+        self.connection.send(Str.encode())
+    
+    def NewInstance(self):
+        self.root.mainloop()
+    def CloseConnection(self):
+        if type(self.connection) == socket.socket:
+            self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.connection.connect((self.IP, self.port_no))
+            self.connection.send(b"")
+            self.connection.close()
+            
+ins = Client()
+ins.NewInstance()
---
+++ server.py
@@ -0,0 +1,216 @@
+from tkinter import ttk
+from tkinter import *
+import threading
+import socket
+import os
+import pyautogui
+import registry
+from winreg import *
+from time import sleep
+import subprocess
+import keyboard #pip install keyboard
+
+mp={
+    'HKEY_CLASSES_ROOT':HKEY_CLASSES_ROOT,
+    'HKEY_CURRENT_CONFIG':HKEY_CURRENT_CONFIG,
+    'HKEY_CURRENT_USER':HKEY_CURRENT_USER,
+    'HKEY_USERS':HKEY_USERS,
+    'HKEY_LOCAL_MACHINE':HKEY_LOCAL_MACHINE
+}
+class Server(object):
+    def main_form(self):
+        """Creates the interface window"""
+        self.root = Tk()
+        self.root.title("Server")
+
+        #mainframe
+        self.mainframe = ttk.Frame(self.root, padding="25 25 50 50")
+        self.mainframe.grid(column=0, row=0, sticky=(N,W,E,S))
+        self.root.columnconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=1)
+        self.root.resizable(FALSE, FALSE)
+
+        #Open server button
+        self.connectButton = ttk.Button(self.mainframe, text='Open', command=self.threadConnect)
+        self.connectButton.grid(column=1, row=1)
+        self.root.mainloop()
+        pass
+    def threadConnect(self):
+        con=threading.Thread(target=self.Connect)
+        con.start()
+    def Connect(self):
+        port = 1025
+        #addr = '14.230.23.93'
+        addr = socket.gethostbyname(socket.gethostname())
+        Label(self.mainframe,text=addr+':'+str(port)).grid(column=1,row=2)
+        print(addr, port)
+        self.connectButton['text'] = 'Close'
+        self.connectButton['command'] = self.Close
+        global s
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+        s.bind((addr, port))
+        s.listen()
+        while True:
+            self.conn, self.target_addr = s.accept()
+            data=self.conn.recv(1024)
+            print(data)
+            if not data:
+                break
+            self.magicFunction(data)
+    #Ham nay nhan lenh tu client
+    def magicFunction(self,Str):
+        if Str.decode()=='Hello':
+            print('Hello')
+        elif Str.decode().find('SHUTDOWN')!=-1:
+            #Commands the server to shut down
+            try:
+                a = Str.decode().split()
+                cmd = Str.decode()
+                if len(a) == 2:
+                    cmd='shutdown -s -t ' + a[1]
+                else:
+                    cmd='shutdown -s'
+                os.system(cmd)
+            except Exception as e:
+                self.conn.send("Invalid command: " + str(e))
+        elif Str.decode() == 'CAPSCR':
+            pyautogui.screenshot().save('scr.png')
+            send = open('scr.png','rb')
+            while True:
+                data=send.read(1024)
+                if not data:
+                    break
+                self.conn.sendall(data)
+        
+        elif Str.decode() == 'SHWPRC':
+            #Commands the server to send the file consisting of running processes
+            os.system("wmic process get Name, ProcessId, ThreadCount >list.txt")
+            send = open('list.txt', 'r')
+            while True:
+                data = send.readline()
+                print(data)
+                if not data:
+                    self.conn.send('STOPRIGHTNOW'.encode())
+                    break
+                self.conn.sendall(data.strip().encode())
+        elif Str.decode() == 'SHWPRCAPP':
+            tmp = subprocess.check_output("powershell gps | where {$_.MainWindowTitle} | select Name,Id,@{Name='ThreadCount';Expression={$_.Threads.Count}}")
+            arr = tmp.split()[6:]
+            print(arr)
+            for i in range(0,len(arr)//3,1):
+                plusStr=str(arr[3*i].decode()+' '+arr[3*i+1].decode()+' '+arr[3*i+2].decode()+' ')
+                self.conn.send(plusStr.encode())
+            self.conn.send('STOPRIGHTNOW'.encode())
+        elif Str.decode().find('GETVALUE')!=-1:
+            arr = Str.decode().split(' ')
+            brr = arr[1].split('\\',1)
+            self.conn.send(str(registry.getValue(mp[brr[0]],brr[1],arr[2])).encode())
+        elif Str.decode().find('DELETEVALUE')!=-1:
+            arr = Str.decode().split(' ')
+            brr = arr[1].split('\\',1)
+            if registry.deleteValue(mp[brr[0]],brr[1],arr[2]):
+                self.conn.send('Completed'.encode())
+            else:
+                self.conn.send('Failure'.encode())
+        elif Str.decode().find('CREATEKEY')!=-1:
+            arr = Str.decode().split(' ')
+            if registry.createKey(arr[1])==True:
+                self.conn.send('Completed'.encode())
+            else:
+                self.conn.send('Failure'.encode())
+        elif Str.decode().find('DELETEKEY')!=-1:
+            arr = Str.decode().split(' ')
+            if registry.deleteKey(arr[1])==True:
+                self.conn.send('Completed'.encode())
+            else:
+                self.conn.send('Failed'.encode())
+        elif Str.decode().find('SETVALUE')!=-1:
+            arr = Str.decode().split('%')
+            brr = arr[1].split('\\',1)
+            try:
+                if registry.setValue(mp[brr[0]],brr[1],arr[2],arr[3],arr[4]) == True:
+                    self.conn.send('Completed'.encode())
+                else:
+                    self.conn.send('Failed'.encode())
+            except:
+                self.conn.send('Failed'.encode())
+        elif Str.decode().find('KILLAPP') != -1:
+            name = str(Str.decode().split()[1])
+            try:
+                subprocess.check_output('powershell Stop-Process -ID '+ name +' -Force')            
+                self.conn.send('TRUE'.encode())
+            except:
+                self.conn.send('FALSE'.encode())
+                pass
+        elif Str.decode().find('KILL') != -1:
+            PID = int(Str.decode().split()[1])
+            try:
+                os.kill(PID, 9)
+                self.conn.send('TRUE'.encode())
+            except:
+                self.conn.send('FALSE'.encode())
+                pass
+        elif Str.decode().find('START') != -1:
+            try:
+                os.system(Str.decode())
+                self.conn.send('TRUE'.encode())
+            except:
+                self.conn.send('FALSE'.encode())
+                pass
+        elif Str.decode() == 'KEYLOG':
+            bep = threading.Thread(target=self.startKeylogging)
+            bep.start()
+        elif Str.decode() == 'KEYSTOP':
+            self.stopKeylogging()
+        else:
+            f_bin=open('testing.reg','wb+')
+            f_bin.write(Str)
+            f_bin.close()
+            if len(Str)<1024:
+                registry.importRegistry(filepath=r'E:\Python\Socket_Midterm\testing.reg')
+                
+
+    #ATTRIBUTES AND METHODS SPECIFICALLY FOR KEYLOGGING:
+    __interval = 5
+    __log = ''
+    __noch = 0
+    def __callback(self, event):
+        name = event.name
+        if len(name) > 1:
+                if name == 'space':
+                    name = ' '
+                elif name == 'enter':
+                    name = '[ENTER]\n'
+                elif name == 'decimal':
+                    name = '.'
+                else:
+                    name = name.replace(" ", "_")
+                    name = f"[{name.upper()}]"
+        self.__log += name
+    def __report(self):
+        if self.__log:
+            self.conn.send(self.__log[self.__noch:].encode())
+            self.__noch = len(self.__log)
+        timer = threading.Timer(interval=self.__interval, function=self.__report)
+        timer.daemon = True
+        timer.start()
+    def startKeylogging(self):
+        keyboard.on_release(self.__callback)
+        self.__report()
+        keyboard.wait()
+    #TODO: Unhook
+    def stopKeylogging(self):
+        keyboard.unhook(self.__callback)
+    ##################################################################################
+    def Close(self):
+        s.close()
+        close_it=threading.Thread(target=self.root.destroy,daemon=True)
+        close_it.start()
+ins=Server()
+mainz=threading.Thread(target=ins.main_form)
+try:
+    mainz.start()
+except:
+    ins.Connect()
+    pass
