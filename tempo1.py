1983fb74e8b8cc9acf6e408247f6c2389f69cc06
Quang <lenhatquang97@gmail.com>
2021-10-18 10:10:22 +0700

    Delete temp file

--- tempo1.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-tmp = subprocess.check_output("powershell gps | where {$_.MainWindowTitle} | select Name,Id,@{Name='ThreadCount';Expression={$_.Threads.Count}}")
-value = tmp.decode('utf-8').split()[6:]
-print(value)
