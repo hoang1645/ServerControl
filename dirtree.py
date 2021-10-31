a4509ae26876c176a308eac8a589dc8da5bc846f
hoang1645 <60732952+hoang1645@users.noreply.github.com>
2021-10-31 10:35:45 +0700

    bugfixes

--- dirtree.py
+++ dirtree.py
@@ -62,7 +62,7 @@ class FileTree():
         print(partitions)
         assert type(partitions) == list
         for p in partitions:
-            self.fileTree.insert("", "end", p, text=p, values = (p, ))
+            self.fileTree.insert("", "end", p, text=p, values = (p, "dir"))
             # levels = []
             # paths = data[p]
             # for path in paths:
@@ -112,6 +112,9 @@ class FileTree():
         
         if (self.item):
             serverPath = self.fileTree.item(self.item, 'values')[0]
+            type = self.fileTree.item(self.item, 'values')[1]
+            if type == "file":
+                return
             self.conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             self.conn.connect((self.ip, self.port))
             self.conn.send(("GET " + serverPath).encode(encoding='utf8'))
@@ -123,10 +126,12 @@ class FileTree():
                 if (len(d)) < 1024:
                     break
             List = json.loads(data)
-            for item in List:
+            for item, type in List:
                 fullPath = os.path.join(serverPath, item)
-                self.fileTree.insert(serverPath, "end", fullPath, text=item, values=(fullPath, ))
-
+                try:
+                    self.fileTree.insert(serverPath, "end", fullPath, text="<" + type + "> " + item, values=(fullPath, type))
+                except TclError:
+                    pass
     def Copy(self):
         dir = filedialog.askdirectory()
         name = self.fileTree.item(self.item, 'text')
@@ -155,7 +160,7 @@ class FileTree():
     def sendSignal(self):
         self.conn.send("DIRSHW".encode())
         self.data = self.conn.recv(1024).decode()
-        print(self.data)
+
     
 
 
@@ -176,8 +181,8 @@ class FileTree():
                 ret.append('{}{}'.format(subindent, f))
         return ret
 
-# ft = FileTree(None, '10.2.0.2', 1025)
+ft = FileTree(None, '10.2.0.2', 1025)
 # filepath =  "E:\\list.txt"
 # with open(filepath, "w", encoding="utf8") as ofile:
 #     ft.list_files(ofile)
-# ft.startInstance()
+ft.startInstance()
